c21411fc1dca3b839620798a5b362b404cfe6dee
koray <koray@e-kalite.com.tr>
2020-04-01 18:55:10 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React  from 'react'
+import {Text, StyleSheet} from 'react-native'
+
+const App = () => {
+  return (
+    <>
+      <Text></Text>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default App
